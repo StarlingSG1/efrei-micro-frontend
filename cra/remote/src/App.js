c831e041a9f5fbec0a4103a5711f5eca3c540189
Jérémie <barriere.jeremie@gmail.com>
2024-06-12 12:32:16 +0200

    [add] AWS

--- cra/remote/src/App.js
+++ cra/remote/src/App.js
@@ -1,20 +1,66 @@
-import React from "react"
+import React, { useEffect, useState } from 'react';
 import LocalButton from './Button';
 import { Title } from './components/Title';
+import AWS from 'aws-sdk';
+
 const Header = React.lazy(() => import('sharedComponents/Header'));
 const Footer = React.lazy(() => import('sharedComponents/Footer'));
-const App = () => (
-  <div className={'app-container'}>
-    <React.Suspense fallback={"Loading Header"}>
-      <Header/>
-    </React.Suspense>
-    <main className={'app-main-container'}>
-      <Title title={"Voici le Remote"}/>
-    </main>
-    <React.Suspense fallback={"Loading Footer"}>
-      <Footer/>
-    </React.Suspense>
-  </div>
-);
+
+const BUCKET_NAME = "efrei-microfrontend";
+
+
+const s3 = new AWS.S3({
+  accessKeyId: "AKIAYOHRW6AXOB7DYCZC",
+  secretAccessKey: "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",
+  region: "eu-west-3",
+});
+
+const App = () => {
+
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const params = {
+          Bucket: BUCKET_NAME,
+          Prefix: '', // Optionally, you can specify a folder within the bucket
+        };
+
+        const data = await s3.listObjectsV2(params).promise();
+        const imageKeys = data.Contents.map(item => item.Key);
+        const imageUrls = imageKeys.map(key => `https://${BUCKET_NAME}.s3.amazonaws.com/${key}`);
+
+        setImages(imageUrls);
+      } catch (error) {
+        console.error('Error fetching images from S3:', error);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
+  return (
+
+
+    <div className={'app-container'}>
+      <React.Suspense fallback={'Loading Header'}>
+        <Header />
+      </React.Suspense>
+      <main className={'app-main-container'}>
+        <Title title={'Voici le Remote'} />
+        <div className="image-gallery">
+          {images.map((url, index) => (
+            <img key={index} src={url} alt={`S3 Image ${index}`} />
+          ))}
+        </div>
+      </main>
+
+      <React.Suspense fallback={'Loading Footer'}>
+        <Footer />
+      </React.Suspense>
+    </div>
+  )
+}
 
 export default App;
--- cra/uploadPDF/src/App.js
+++ cra/uploadPDF/src/App.js
@@ -1,20 +1,67 @@
-import React from "react"
-import LocalButton from './Button';
-import { Title } from './components/Title';
+import React, { useState } from 'react';
+import AWS from 'aws-sdk';
 const Header = React.lazy(() => import('sharedComponents/Header'));
 const Footer = React.lazy(() => import('sharedComponents/Footer'));
-const App = () => (
+const App = () => {
+
+const [file, setFile] = useState(null);
+const handleFileChange = (e) => {
+  const file = e.target.files[0];
+  setFile(file);
+};
+
+  const uploadFile = async () => {
+    const S3_BUCKET = "efrei-microfrontend";
+    const REGION = "eu-west-3";
+
+    AWS.config.update({
+      accessKeyId: "AKIAYOHRW6AXOB7DYCZC",
+      // accessKeyId: process.env.REACT_APP_BUCKET_KEY_ID,
+      secretAccessKey: "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",
+      // secretAccessKey: process.env.REACT_APP_BUCKET_KEY_SECRET,
+    });
+    const s3 = new AWS.S3({
+      params: { Bucket: S3_BUCKET },
+      region: REGION,
+    });
+
+    const params = {
+      Bucket: S3_BUCKET,
+      Key: file.name,
+      Body: file,
+    };
+
+    var upload = s3
+      .putObject(params)
+      .on("httpUploadProgress", (evt) => {
+        console.log(
+          "Uploading " + parseInt((evt.loaded * 100) / evt.total) + "%"
+        );
+      })
+      .promise();
+
+    await upload.then((err, data) => {
+      console.log(err);
+      alert("File uploaded successfully.");
+    });
+  };
+
+return (
   <div className={'app-container'}>
     <React.Suspense fallback={"Loading Header"}>
       <Header/>
     </React.Suspense>
     <main className={'app-main-container'}>
-      <Title title={"Voici le Remote"}/>
+      <div>
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={uploadFile}>Upload</button>
+      </div>
     </main>
     <React.Suspense fallback={"Loading Footer"}>
-      <Footer/>
+      <Footer />
     </React.Suspense>
   </div>
-);
+)
+}
 
 export default App;
